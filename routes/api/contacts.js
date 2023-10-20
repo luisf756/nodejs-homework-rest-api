284b6b0a08923830ee1b284c5cda2d5c45aea8c0
luisf756 <luchof756@gmail.com>
2023-10-19 21:57:47 -0500

    add auth to concacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,17 +8,17 @@ const { ctrlWrapper } = require(`${basedir}/helpers`);
 
 const router = express.Router()
 
-router.get('/', ctrlWrapper(ctrl.getAllContacts));
+router.get('/', auth, ctrlWrapper(ctrl.getAllContacts));
 
-router.get('/:contactId', ctrlWrapper(ctrl.getContactById));
+router.get('/:contactId', auth, ctrlWrapper(ctrl.getContactById));
 
-router.post('/', ctrlWrapper(ctrl.addContact));
+router.post('/', auth, ctrlWrapper(ctrl.addContact));
 
-router.delete('/:contactId', ctrlWrapper(ctrl.removeContact));
+router.delete('/:contactId', auth, ctrlWrapper(ctrl.removeContact));
 
-router.put('/:contactId', ctrlWrapper(ctrl.updateContactById));
+router.put('/:contactId', auth, ctrlWrapper(ctrl.updateContactById));
 
-router.patch('/:contactId/favorite', ctrlWrapper(ctrl.updateStatus));
+router.patch('/:contactId/favorite', auth, ctrlWrapper(ctrl.updateStatus));
 
 module.exports = router;
 
